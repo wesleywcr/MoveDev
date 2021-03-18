ccb019c44af0ebc7d8736858857b0950391470c1
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2021-03-18 16:54:53 -0300

    styled page sing in

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,20 +6,29 @@ export default function Home() {
   const [session, loading] = useSession()
 
   return (
-    <div className={styles.container}>
-      <Link href="/home">
-        <a >Home</a>
-      </Link>
+    <div className={styles.containerLogin}>
+      <img src='simbolo.svg' alt="" />
 
       <div>
         {!session && <>
-          <h1> Bem-vindo</h1>
+          <span><img src='logo-full.svg' alt="Logo" /></span>
+          <h1> Bem vindo</h1>
           <p>Faça o login com seu Github para começar </p>
-          <button className={styles.buttonSingOut} onClick={() => signIn('github', { callbackUrl: 'http://localhost:3000/Home' })}>LOGIN</button>
+          <button
+            className={styles.buttonSingIn}
+            onClick={() =>
+              signIn('github', { callbackUrl: 'http://localhost:3000/Home' })}>
+            <span><img src='icons/github.svg' alt="" /></span>
+            LOGIN</button>
         </>}
+
         {session && <>
           <p>{session.user.name} Deseja sair ?</p>
-          <button className={styles.buttonSingOut} onClick={() => signOut()}>SAIR</button>
+          <button
+            className={styles.buttonSingOut}
+            onClick={() => signOut()}>
+            SAIR
+            </button>
         </>}
       </div>
 
